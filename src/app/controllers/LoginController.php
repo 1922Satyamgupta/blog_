0df215b4fa50d4b9940a2de3722dacd539304cfd
satyam <raj@gmail.com>
2022-03-22 11:21:34 +0530

    first commit

--- src/app/controllers/LoginController.php
+++ src/app/controllers/LoginController.php
@@ -6,6 +6,37 @@ class LoginController extends Controller
 {
     public function indexAction()
     {
-        //return '<h1>Hello!!!</h1>';
+        $this->view->user = Users::find();
     }
+    public function loginAction() {
+        // $user = Users::find(["email='$email'"]);
+        $email = $this->request->getPost('email');
+        $pass = $this->request->getPost('password');
+        
+       
+        $user = Users::findFirst(['email = "'.$email.'"']);
+        //  echo $user->email;
+        //  die();
+        if ($email == "" && $pass =="") {
+         
+            echo "insert email and password";
+        }
+        elseif ($user->email == $email && $user->password == $pass && $user->role == 'users' && $user->status == 'approved') {
+            header("Location: http://localhost:8080/blog/index");
+        }
+        elseif ($user->email == $email && $user->password == $pass && $user->role == 'users' && $user->status == 'unapproved') {
+            echo "Your status is not approved. call to admin!!" ;
+        }
+        elseif ($user->email == $email && $user->password == $pass && $user->role == 'admin' && $user->status == 'approved') {
+            header("Location: http://localhost:8080/index");
+        }
+        elseif ($user->email == $email && $user->password == $pass && $user->role == 'admin' && $user->status == 'unapproved') {
+            echo "Your status is not approved. call to admin!!" ;
+        }
+        else {
+            header("Location: http://localhost:8080/login/index");
+        }
+    }
+
+        
 }
